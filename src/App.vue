d74e8a77aac48766ed8bd0e8c4c2b33987490e64
Niya Shiyas <niyashiyas03@gmail.com>
2023-08-20 13:48:10 +0530

    made structure

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Todo from "./components/Todo.vue"
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>To-Do List</h1>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Todo/>
   </main>
 </template>
 
@@ -22,10 +19,6 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 @media (min-width: 1024px) {
   header {
---
+++ src/components/Todo.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+    <h1 class= "header">To Do versions</h1>
+</template>
